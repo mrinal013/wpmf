e097d6c154134d22570a2a74b9f974d693889aad
Mrinal Haque <mrinalhaque99@gmail.com>
2019-10-31 16:38:01 +0600

    init

---
+++ includes/class-code-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mrinalbd.com/
+ * @since      1.0.0
+ *
+ * @package    Code
+ * @subpackage Code/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Code
+ * @subpackage Code/includes
+ * @author     Mrinal Haque <mrinalhaque99@gmail.com>
+ */
+class Code_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-code-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mrinalbd.com/
+ * @since      1.0.0
+ *
+ * @package    Code
+ * @subpackage Code/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Code
+ * @subpackage Code/includes
+ * @author     Mrinal Haque <mrinalhaque99@gmail.com>
+ */
+class Code_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
